dac1177fb252fcaf8158cf4b40959b1b58abcd36
Colin Turner <colinturner92@gmail.com>
2017-03-06 16:37:24 +0000

    add buzz test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns 7 when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
 end
