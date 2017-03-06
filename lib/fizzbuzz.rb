01e4ead89b0d5b509ad97b0bd15434765e12932b
Colin Turner <colinturner92@gmail.com>
2017-03-06 17:25:15 +0000

    refactored one-line solution as comment

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -11,3 +11,12 @@ def fizzbuzz(number)
     number
   end
 end
+
+
+# Following the CodeWars tradition of 'increasing legibility' by decreasing script length, here is our alternative solution:
+
+=begin
+def fizzbuzz(number)
+  number == 0 ? 0 : (number % 5 == 0 && number % 3 == 0) ? 'fizzbuzz' : (number % 5 == 0) ? 'buzz' : (number % 3 == 0) ? 'fizz' : number
+end
+=end
